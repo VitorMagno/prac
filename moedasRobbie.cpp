3158bd555cd611786e32b2490e33472d2f3a5407
Vitor Magno <vmcg@ic.ufal.br>
2023-06-21 12:00:43 -0300

    em andamento

--- moedasRobbie.cpp
+++ moedasRobbie.cpp
@@ -12,12 +12,39 @@ typedef long long ll;
 const int INF = 0x3f3f3f3f;
 const ll LINF = 0x3f3f3f3f3f3f3f3fll;
 
+bool ehPrimo(int t){
+    for(int i = 0; i < t; i++){
+        bool primo = true;
+        ll n;
+        cin >> n;
+        for(ll j = 2; j*j <= n; j++){
+            if(n % j == 0){
+                primo = false;
+                break;
+            }
+        }
+        return primo;
+    }
+}
+
 int main(int argc, char* argv[]){ //_
     vector<int> v(20,0);
-    int n;
-    while(cin >> n){
-        for(int i = 0; i < n; i++){
-            
+    int m;
+    while(cin >> m){
+        int sum = 0;
+        for(int i = 0; i < m; i++){
+            cin >> v[i];
+        }
+        int salto;
+        cin >> salto;
+        for(int i = m-1; i >= 0; i-=salto){
+            sum+=v[i];
+        }
+        bool teste = ehPrimo(sum);
+        if(teste){
+            cout << "You’re a coastal aircraft, Robbie, a large silver aircraft." << endl;
+        }else{
+            cout << "Bad boy! I’ll hit you." << endl;
         }
     }
     return 0;
